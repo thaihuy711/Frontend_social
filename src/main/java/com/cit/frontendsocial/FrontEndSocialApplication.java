24b6e51427ab8f85cce07faf3eb6f3c180ea6201
thaihuy711 <thaihuy7191112@gmail.com>
2017-12-18 17:06:22 +0700

    upload

---
+++ src/main/java/com/cit/frontendsocial/FrontEndSocialApplication.java
@@ -0,0 +1,12 @@
+package com.cit.frontendsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndSocialApplication.class, args);
+	}
+}
